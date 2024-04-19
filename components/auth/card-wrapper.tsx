9b7c65d5b7b19e6ffb4530f69fdfcec938ddb44f
Dan <daniel.smirnov17@gmail.com>
2024-04-19 01:04:41 +0100

    added Login form CardWrapper & low level reusable components

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import { BackButton } from "./back-buton";
+import { Header } from "./header";
+import { Social } from "./social";
+
+interface CardWrapperProps {
+    children: React.ReactNode;
+    headerLaberl: string;
+    backButtonLabel: string;
+    backButtonHref: string;
+    showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+    children,
+    headerLaberl,
+    backButtonLabel,
+    backButtonHref,
+    showSocial,
+}: CardWrapperProps) => {
+    return (
+        <Card className="w-[400px] shadow-md">
+            <CardHeader>
+                <Header label={headerLaberl} />
+            </CardHeader>
+            <CardContent>{children}</CardContent>
+            {showSocial && (
+                <CardFooter>
+                    <Social />
+                </CardFooter>
+            )}
+            <CardFooter>
+                <BackButton label={backButtonLabel} href={backButtonHref} />
+            </CardFooter>
+        </Card>
+    );
+};
