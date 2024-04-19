a95e8225a3def3a13dd5da03f8cf52fae9728fed
Dan <daniel.smirnov17@gmail.com>
2024-04-19 22:10:46 +0100

    Add next-auth & setup all the middleware & config for auth routes

---
+++ middleware.ts
@@ -0,0 +1,35 @@
+import authConfig from "./auth.config"
+import NextAuth from "next-auth"
+import { apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes } from "./routes"
+
+const { auth: middleware } = NextAuth(authConfig)
+
+export default middleware((req) => {
+    const { nextUrl } = req
+    const isLoggedIn = !!req.auth
+
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+    const isAuthRouet = authRoutes.includes(nextUrl.pathname)
+
+    if (isApiAuthRoute) {
+        return null
+    }
+
+    if (isAuthRouet) {
+        if (isLoggedIn) {
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+        }
+        return null
+    }
+
+    if (!isLoggedIn && !isPublicRoute) {
+        return Response.redirect(new URL("auth/login", nextUrl))
+    }
+
+    return null
+})
+
+export const config = {
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
