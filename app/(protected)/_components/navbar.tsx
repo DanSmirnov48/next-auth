548f379b3963de69f8f48a9a88e016c2877dc2e5
Dan <daniel.smirnov17@gmail.com>
2024-04-29 00:03:23 +0100

    added navbar & user button and layout

---
+++ app/(protected)/_components/navbar.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import UserButton from '@/components/auth/user-button'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+export const Navbar = () => {
+    const pathname = usePathname()
+    return (
+        <nav className='bg-secondary flex justify-between items-center p-4 rounded-xl w-[600px] shadow-sm'>
+            <div className='flex gap-x-2'>
+                <Button asChild variant={pathname === '/settings' ? 'default' : 'outline'}>
+                    <Link href="/settins">
+                        Settings
+                    </Link>
+                </Button>
+            </div>
+            <UserButton />
+        </nav>
+    )
+}
---
+++ components/auth/user-button.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { FaUser } from "react-icons/fa";
+import { useCurrentUser } from "@/hooks/useCurrentuser";
+import LogoutButton from "./logout-button";
+import { LogOut } from "lucide-react";
+
+const UserButton = () => {
+    const user = useCurrentUser()
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar>
+                    <AvatarImage src={user?.image || ""} />
+                    <AvatarFallback className="bg-sky-500">
+                        <FaUser className="text-white" />
+                    </AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <LogoutButton>
+                    <DropdownMenuItem>
+                        <LogOut className="w-4 h-4 mr-2" />
+                        Logout
+                    </DropdownMenuItem>
+                </LogoutButton>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
+
+export default UserButton;
